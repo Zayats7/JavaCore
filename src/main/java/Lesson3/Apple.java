4d7fcc9944f85ab42e6bd23632b88b71f875f767
Alexei <azaytcev777@gmail.com>
2021-09-07 17:27:42 +0300

    lesson 3 is done

---
+++ src/main/java/Lesson3/Apple.java
@@ -0,0 +1,10 @@
+package Lesson3;
+
+public class Apple extends Fruit
+{
+    @Override
+    float getWeight()
+    {
+        return 1.0f;
+    }
+}
---
+++ src/main/java/Lesson3/Fruit.java
@@ -0,0 +1,6 @@
+package Lesson3;
+
+public abstract class Fruit
+{
+    abstract float getWeight();
+}
